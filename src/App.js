7a4fe8c5149025ee1de2a7ebf3cf1e14a575ac39
SravanKumar35 <kumarsravan9182@gmail.com>
2022-07-06 16:11:27 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Template from "./templates/template";
+import { useEffect, useRef, useState } from "react";
+import { useReactToPrint } from "react-to-print";
 
 function App() {
+  const [first, setFirst] = useState(false);
+  const [second, setSecond] = useState(false);
+  const [third, setThird] = useState(false);
+
+  const [canDownload, setCanDownload] = useState(false);
+
+  const toggleCanDownload = () => {
+    setCanDownload(!canDownload);
+    if (canDownload) {
+      setFirst(false)
+      setSecond(false)
+      setThird(false)
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="templates">
+        <div className="template">
+          <Template
+            value={1}
+            print={first && canDownload}
+            toggleCanDownload={toggleCanDownload}
+          />
+        </div>
+        <div className="template">
+          <Template
+            value={2}
+            print={second && canDownload}
+            toggleCanDownload={toggleCanDownload}
+          />
+        </div>
+        <div className="template">
+          <Template
+            value={3}
+            print={third && canDownload}
+            toggleCanDownload={toggleCanDownload}
+          />
+        </div>
+      </div>
+
+      <div className="checkboxes">
+        <div>
+          <span>Please check this box to download: </span>
+          <input
+            type="checkbox"
+            name="template1"
+            value="Template1"
+            checked={first}
+            onChange={() => {
+              setFirst(!first);
+            }}
+          />
+        </div>
+        <div>
+          <span>Please check this box to download: </span>
+          <input
+            type="checkbox"
+            name="template2"
+            value="Template2"
+            checked={second}
+            onChange={() => {
+              setSecond(!second);
+            }}
+          />
+        </div>
+        <div>
+          <span>Please check this box to download: </span>
+          <input
+            type="checkbox"
+            name="template3"
+            value="Template3"
+            checked={third}
+            onChange={() => {
+              setThird(!third);
+            }}
+          />
+        </div>
+      </div>
+
+      <button onClick={toggleCanDownload}>Download</button>
     </div>
   );
 }
